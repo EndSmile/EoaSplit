ca385702b470f711e33692c49095dda618b53b4d
EndSmile <745472638@qq.com>
2017-06-26 10:39:21 +0800

    完善注释

--- common/src/main/java/com/ldy/common/modulecommunication/event/NotificationEvent.java
+++ common/src/main/java/com/ldy/common/modulecommunication/event/NotificationEvent.java
@@ -2,7 +2,7 @@ package com.ldy.common.modulecommunication.event;
 
 /**
  * Created by ldy on 2017/6/22.
- * 通知事件，纯粹的事件，用于模块间的消息传递
+ * 通知事件，纯粹的事件，用于模块间的消息传递，可能含有需传递的数据
  */
 
 public interface NotificationEvent extends Event{
